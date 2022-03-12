2c3d18d6e82fbc498f78c2cbdd5119460c8c9f56
fernandocandidodesiqueirajunior <fjunior@psgtecnologiaaplicada.com.br>
2022-03-11 23:45:48 -0400

    icon github

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,22 @@
+import {ReactComponent as GitHubIcon} from './assets/img/github.svg';
+
 function App() {
   return (
-    <h1>Página principal</h1>
+    <header>
+      <nav>
+        <div>
+          <h1>DS Movie</h1>
+          <a href="https://github.com/fernando-candido">
+          <div>
+            <GitHubIcon />
+            <p>/fernando-candido</p>
+          </div>
+          </a>
+        </div>
+
+      </nav>
+
+    </header>
   );
 }
 
