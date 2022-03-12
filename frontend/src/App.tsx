6caec5166691b7d228919c1c4c325266e846bf61
fernandocandidodesiqueirajunior <fjunior@psgtecnologiaaplicada.com.br>
2022-03-12 14:23:00 -0400

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,24 @@
-import Navbar from "./components/NavBar";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Form from 'pages/Form';
+import Listing from 'pages/Listing';
+import Navbar from "components/NavBar";
 
 function App() {
   return (
-    <Navbar />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
